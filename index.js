f00d2c3a472e76383b99faceea39d00784de21f0
Quillbert <quillbert.quillbert@gmail.com>
2021-05-10 16:33:41 -0400

    rearranged files. oops.

--- index.js
+++ index.js
@@ -1,120 +1,107 @@
+//Importing Dependencies
 const {spawn, exec} = require('child_process');
-var express = require('express');
+const express = require('express');
+const socket = require('socket.io');
+const fs = require('fs');
+
+//Initializing Servers
 var app = express();
 var server = app.listen(process.env.PORT || 80);
 app.use(express.static(__dirname + "/public"));
-var socket = require('socket.io');
+
 var io = socket(server);
-const fs = require('fs');
 console.log("socket started");
 
-
-fs.readFile("./tests/Prob01/Prob01.in.txt", 'utf8', (err, data) => {
-	var text = "";
-	if(err) {
-		console.error(err);
-		return;
-	}
-	text = data;
-	//console.log(text);
-});
-
+//Handling Server Messages
 io.on("connection", function(socket) {
 	socket.on("file", function(data) {
-		console.log("recieved");
-		exec("mkdir " + socket.id, (err) => {
-		  if (err) {
-			console.error(err);
-			return;
-		  }
-			fs.writeFile("./" + socket.id+"/" + data.name, data.text, err => {
-			  if (err) {
-				console.error(err);
-				return;
-			  }
-			  run(socket.id, data.name);
-			});
-		});
+		create(socket.id, data);
 	});
 });
-function run(data, name) {
-	var ps = spawn("javac", [name], {cwd: './'+ data + '/'});
 
-	var done = false;
+//Create a folder for java files, then continue execution
+function create(id, data) {
+	exec("mkdir " + id, (err) => {
+		if (err) {
+			console.error(err);
+			return;
+		}
+		write(id, data);
+	});
+}
+
+//Saves the java file, then continues to compilation
+function write(id, data) {
+	fs.writeFile("./" + id +"/" + data.name, data.text, err => {
+		if(err) {
+			console.error(err);
+			return;
+		}
+		compile(id, data.name);
+	});
+}
+
+//Compiles the java file, then continues to run it
+function compile(id, name) {
+	var ps = spawn("javac", [name], {cwd: './' + id + '/'});
 
 	ps.stdout.on('data', (data) => {
-		var s = data + "";
-		if(s.trim().length > 0) {
-			console.log("Java says: " + s.trim());
-		}
+		console.log("Javac: " + data);
 	});
 
-
 	ps.stderr.on('data', (data) => {
-	  console.error(`ps stderr: ${data}`);
+		console.error("compile error: " + data);
 	});
 
 	ps.on("close", (close) => {
 		if(close == 0) {
-			console.log("compiled");
-			var output = "";
-			var ps1 = spawn("java", [name.substring(0, name.lastIndexOf("."))], {cwd: './' + data + '/'});
-			//var ps1 = spawn("java", ["Test"], {cwd: './test/'});
-			ps1.stdout.on('data', (data) => {
-				var s = data + "";
-				output += data;
-				if(s.trim().length > 0) {
-					//console.log("Java says: " + s.trim());
-				}
-			});
-
-			ps1.stderr.on('data', (data) => {
-			  console.error(`ps stderr: ${data}`);
-			});
-			
-			ps1.on("close", (close) => {
-				exec("rm -rf " + data);
-				check(output);
-			});
-
-
-			/*ps1.stdin.write(
-				"1\n"+
-				"6 2\n"+
-				"0 1\n"+
-				"2 1\n"+
-				"3 1\n"+
-				"0 0\n"+
-				"1 0\n"+
-				"2 0\n"+
-				"1 1\n"+
-				"3 0\n"
-			);*/
-			send(ps1);
+			run(id, name);
 		}
 	});
 }
 
-function send(ps) {
-	fs.readFile("./tests/Prob01/Prob01.in.txt", 'utf8', (err, data) => {
-	var text = "";
-	if(err) {
-		console.error(err);
-		return;
-	}
-	text = data;
-	ps.stdin.write(text);
-});
+//Runs the code, the continues to send input and check
+function run(id, name) {
+	var ps = spawn("java", [name.substring(0, name.lastIndexOf("."))], {cwd: './' + id + '/'});
+	
+	var output = "";
+
+	ps.stdout.on('data', (data) => {
+		output += data;
+	});
+
+	ps.stderr.on('data', (data) => {
+		console.error("java error: " + data);
+	});
+
+	ps.on("close", (close) => {
+		exec("rm -rf " + id);
+		check(output);
+	});
+
+	send(ps);
 
 }
 
+//Sends the input from the file to java stdin
+function send(ps) {
+	fs.readFile("./tests/Prob01/Prob01.in.txt", 'utf8', (err, data) => {
+		if(err) {
+			console.error(err);
+			return;
+		}
+		ps.stdin.write(data);
+	});
+}
+
+//Checks whether the output matches the intended output
 function check(input) {
 	fs.readFile("./tests/Prob01/Prob01.out.txt", 'utf8', (err, data) => {
 		if(err) {
 			console.error(err);
 			return;
 		}
-		data = data.replace(/\r/g, '');
+		data = data.replace(/\r/g, ''); //Standardize line endings
 		console.log(String(data) == String(input));
 	});
 }
